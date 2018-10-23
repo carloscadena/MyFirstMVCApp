1c96d9f88439d4872e02133eba0f8cee37bb1177
carlos cadena <carloscadena@live.com>
2018-10-23 12:19:04 -0700

    Add project files.

---
+++ MyFirstMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            // Looks for the Razor View Page located in
+            // Views -> Home -> Index.cshtml
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(int startDate, int endDate)
+        {
+            // Redirect to another Action. 
+            // we create a 
+            return RedirectToAction("Result", new { startDate, endDate  });
+        }
+
+        public ViewResult Result(int startDate, int endDate)
+        {
+            return View();
+        }
+    }
+}
